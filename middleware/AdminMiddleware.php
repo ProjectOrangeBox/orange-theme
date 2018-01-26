e7af678a8352cb7568d59c7dbb6a8f02a04b7cdb
Don Myers <donmyers@quadratec.com>
2018-01-26 15:04:17 -0500

    remove auto add

--- middleware/AdminMiddleware.php
+++ middleware/AdminMiddleware.php
@@ -13,13 +13,6 @@ class AdminMiddleware extends Middleware_base {
 	public function __construct() {
 		$key = 'url::/'.strtolower($this->router->fetch_directory().$this->router->fetch_class(true).'::'.$this->router->fetch_method(true).'~'.$this->router->fetch_request_method());
 
-		if (user::has_role(ADMIN_ROLE_ID)) {
-			$group = filter_human($this->router->fetch_class(true));
-			$description = filter_human($this->router->fetch_directory().$this->router->fetch_request_method().' '.$this->router->fetch_class(true).' '.$this->router->fetch_method(true));
-
-			ci('o_permission_model')->add($key,$group,$description);
-		}
-
 		if (user::cannot($key)) {
 			ci('errors')->display(403);
 		}
