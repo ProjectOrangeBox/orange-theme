0ffd4a72bdbe7c91257b32c1d9c4850b7e22e2d7
Don Myers <donmyers@quadratec.com>
2018-02-12 14:56:32 -0500

    add auto look up for datalist

--- libraries/pear_plugins/Pear_datalist.php
+++ libraries/pear_plugins/Pear_datalist.php
@@ -17,5 +17,17 @@ ci('page')
 pear::attach('datalist',function($name='',$options=[],$value='',$extras=[]) {
 	$extras['class'] .= ' datalist';
 
+	if (is_string($options)) {
+		$options = ci('cache')->page->cache('catalog_datalist_'.$options,function($ci) use ($options) {
+			list($model,$primary_key,$human_column) = explode('.',$options,3);
+
+			return ci($model)->catalog($primary_key,$human_column);
+		});
+	}
+
+	if (!array_key_exists($value,$options)) {
+		$options[$value] = $value;
+	}
+
 	return pear::dropdown($name,$options,$value,$extras);
 });
