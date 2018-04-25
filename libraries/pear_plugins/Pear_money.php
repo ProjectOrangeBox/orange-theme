e785c74e3cf252544a921966f3f1aa5282cd0a4b
Don Myers <donmyers@quadratec.com>
2018-04-25 11:02:36 -0400

    make money look better

--- libraries/pear_plugins/Pear_money.php
+++ libraries/pear_plugins/Pear_money.php
@@ -3,7 +3,7 @@
 class Pear_money extends Pear_plugin {
 
 	public function render($number=null) {
-		return money_format('$%i',$number);
+		return (($number < 0) ? '-' : '').'$'.number_format(abs($number),2);
 	}
 
 }
