374e9e2865760c49be0b61969b8c1b043a7fb9ae
Don Myers <donmyers@quadratec.com>
2018-04-25 13:03:05 -0400

    clean up

--- controllers/traits/admin_controller_trait.php
+++ controllers/traits/admin_controller_trait.php
@@ -1,146 +1,130 @@
 <?php
+
 trait admin_controller_trait {
 /**
- * indexAction
- * Insert description here
+ * Call the controllers default model index method with controllers order by and limit
  *
- *
- * @return
- *
- * @access
- * @static
- * @throws
- * @example
  */
 	public function indexAction() {
 		ci('page')->render(null,['records'=>(($this->controller_model) ? ci($this->controller_model)->index($this->controller_order_by, $this->controller_limit) : [])]);
 	}
 
 /**
- * detailsAction
- * Insert description here
+ * Prepare a view for a new record or edit a current records if a id is provided in the URL
  *
- * @param $id
+ * @param $id mixed - primary id of the controllers default model (optional)
  *
- * @return
- *
- * @access
- * @static
- * @throws
- * @example
  */
 	public function detailsAction($id = null) {
-		($id) ? $this->_edit_record($id) : $this->_new_record();
-		ci('page')->render();
+		$data = ($id) ? $this->_edit_record($id) : $this->_new_record();
+
+		ci('page')->render(null,(array)$data);
 	}
 
 /**
- * indexPostAction
- * Insert description here
+ * Call the controllers default model insert method with the posted form data
  *
- *
- * @return
- *
- * @access
- * @static
- * @throws
- * @example
  */
 	public function indexPostAction() {
-		$this->data['primary_key'] = ci($this->controller_model)->insert(ci('input')->request());
+		$request = ci('input')->request();
+
+		ci('event')->trigger('admin.controller.trait.index_post.'.$this->controller_model,$request,$this->controller_model);
+
+		if ($request) {
+			$this->data['primary_key'] = ci($this->controller_model)->insert($request);
+		}
+
 		$this->_rest_output();
 	}
 
 /**
- * indexPatchAction
- * Insert description here
+ * Call the controllers default model update method with the posted form data
  *
- *
- * @return
- *
- * @access
- * @static
- * @throws
- * @example
  */
 	public function indexPatchAction() {
-		ci($this->controller_model)->update(ci('input')->request());
+		$request = ci('input')->request();
+
+		ci('event')->trigger('admin.controller.trait.index.patch.'.$this->controller_model,$request,$this->controller_model);
+
+		if ($request) {
+			ci($this->controller_model)->update($request);
+		}
+
 		$this->_rest_output();
 	}
 
 /**
- * indexDeleteAction
- * Insert description here
+ * Call the controllers default model delete method with the posted form data's primary index
  *
- *
- * @return
- *
- * @access
- * @static
- * @throws
- * @example
  */
 	public function indexDeleteAction() {
-		ci($this->controller_model)->delete(hex2bin(ci('input')->request(ci($this->controller_model)->get_primary_key())));
+		$request = ci('input')->request();
+		$primary_key = ci($this->controller_model)->get_primary_key();
+		$id = hex2bin($request[$primary_key]);
+
+		ci('event')->trigger('admin.controller.trait.index.delete.'.$this->controller_model,$request,$primary_key,$id,$this->controller_model);
+
+		if ($id) {
+			ci($this->controller_model)->delete($id);
+		}
+
 		$this->_rest_output();
 	}
 
 /**
- * _rest_output
- * Insert description here
+ * Send out json rest data
+ * used by, create, update, delete actions
  *
- *
- * @return
- *
- * @access
- * @static
- * @throws
- * @example
  */
 	protected function _rest_output() {
 		$this->data['ci_errors'] = ci('errors')->as_data();
+
+		ci('event')->trigger('admin.controller.trait.rest.output.'.$this->controller_model,$this->data);
+
 		ci('output')->json($this->data);
 	}
 
 /**
- * _new_record
- * Insert description here
+ * Setup the new records view data
  *
- *
- * @return
- *
- * @access
- * @static
- * @throws
- * @example
+ * @return array
  */
 	protected function _new_record() {
-		ci('page')->data([
+		$data = [
 			'record'          => (object)[],
 			'ci_title_prefix' => 'New',
 			'form_method'     => 'post',
-		]);
+		];
+
+		ci('event')->trigger('admin.controller.trait.data.new.record.'.$this->controller_model,$data);
+
+		return $data;
 	}
 
 /**
- * _edit_record
- * Insert description here
+ * Setup the edit records view data
  *
- * @param $id
+ * @param $id mixed - primary value of the controllers default model of the records to load for editing
  *
- * @return
- *
- * @access
- * @static
- * @throws
- * @example
+ * @return array
  */
 	protected function _edit_record($id = null) {
-		ci('validate')->variable(ci($this->controller_model)->rule(ci($this->controller_model)->get_primary_key(),'rules'), hex2bin($id))->die_on_fail();
-		ci('page')->data([
-			'record'          => ci($this->controller_model)->get(hex2bin($id)),
+		$id = hex2bin($id);
+		$primary_key = ci($this->controller_model)->get_primary_key();
+		$rules = ci($this->controller_model)->rule($primary_key,'rules');
+
+		ci('event')->trigger('admin.controller.trait.validate.edit.record.'.$this->controller_model,$id,$primary_key,$rules);
+
+		ci('validate')->variable($rules,$id)->die_on_fail();
+
+		$data = [
+			'record'          => ci($this->controller_model)->get($id),
 			'ci_title_prefix' => 'Edit',
 			'form_method'     => 'patch',
-		]);
+		];
+
+		ci('event')->trigger('admin.controller.trait.data.edit.record.'.$this->controller_model,$data,$id,$primary_key);
+
+		return $data;
 	}
 }
