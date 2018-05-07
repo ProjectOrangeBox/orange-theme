47804a8130fdf0d2b08414f17ef664190bec7cd3
Don Myers <donmyers@quadratec.com>
2018-05-07 08:45:47 -0400

    trait index

---
+++ controllers/traits/admin_controller_index_trait.php
@@ -0,0 +1,12 @@
+<?php
+
+trait admin_controller_index_trait {
+/**
+ * Call the controllers default model index method with controllers order by and limit
+ *
+ */
+	public function indexAction() {
+		ci('page')->render(null,['records'=>(($this->controller_model) ? ci($this->controller_model)->index($this->controller_order_by, $this->controller_limit) : [])]);
+	}
+
+}
