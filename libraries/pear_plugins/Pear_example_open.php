77c5e7c26a80f71549cb6a529bcef08010949ad2
Don Myers <donmyers@quadratec.com>
2018-05-18 11:13:07 -0400

    add pear examples and pear menu li

---
+++ libraries/pear_plugins/Pear_example_open.php
@@ -0,0 +1,7 @@
+<?php
+
+class Pear_example_open extends Pear_plugin {
+	public function render() {
+		ob_start();
+	}
+}
---
+++ libraries/pear_plugins/Pear_menu_li.php
@@ -0,0 +1,9 @@
+<?php
+
+class Pear_menu_li extends Pear_plugin {
+	public function render($permission=null,$url=null,$text=null) {
+		if (user::can($permission)) {
+			echo '<li><a href="'.site_url($url).'">'.$text.'</a></li>';
+		}
+	}
+}
