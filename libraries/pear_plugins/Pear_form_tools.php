a31f8a771a33aba871154290b8f81519d6bac2ae
Don Myers <donmyers@quadratec.com>
2018-05-18 10:42:02 -0400

    add page_min

--- libraries/pear_plugins/Pear_form_tools.php
+++ libraries/pear_plugins/Pear_form_tools.php
@@ -4,8 +4,8 @@ class Pear_form_tools {
 
 	public function __construct() {
 		ci('page')
-			->js(['/theme/orange/assets/plugins/form-tools/onready_form_tools.min.js','//gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js'])
-			->css('//gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css');
+			->js(['/theme/orange/assets/plugins/form-tools/onready_form_tools'.PAGE_MIN.'.js','//gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle'.PAGE_MIN.'.js'])
+			->css('//gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle'.PAGE_MIN.'.css');
 	}
 
 }
