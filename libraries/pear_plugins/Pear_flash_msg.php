be98762339ec5658781a9998c9750b444a0da275
Don Myers <donmyers@icloud.com>
2018-12-26 15:31:14 -0500

    clean up pear flash msg

--- libraries/pear_plugins/Pear_flash_msg.php
+++ libraries/pear_plugins/Pear_flash_msg.php
@@ -8,34 +8,39 @@ class Pear_flash_msg {
 			->js('/theme/orange/assets/plugins/flash-msg/jquery.bootstrap.flash-msg.js');
 
 		$msgs = [];
-
-		$payload = (array)ci('load')->get_var('wallet_messages');
-
-		$types = [
-			'green'   => 'success',
-			'red'     => 'danger',
-			'yellow'  => 'warning',
-			'blue'    => 'info',
-			'success' => 'success',
-			'error'   => 'danger',
-			'block'   => 'warning',
-			'info'    => 'info',
-			'primary' => 'primary',
-			'warning' => 'warning',
-			'danger'  => 'danger',
-		];
-
-		if (is_array($payload['messages'])) {
-			foreach ($payload['messages'] as $msg) {
-				$msg = ['text'=>$msg['msg'],'stay'=>$msg['sticky'],'type'=>$types[$msg['type']]];
-				if ($msg['sticky'] == true) {
-					$msg['staytime'] = ($payload['pause_for_each'] * ($payload['initial_pause']++));
+		
+		/* get the content from the view variable */
+		$payload = ci('load')->get_var('wallet_messages');
+		
+		if (is_array($payload)) {
+			$types = [
+				'green'   => 'success',
+				'red'     => 'danger',
+				'yellow'  => 'warning',
+				'blue'    => 'info',
+				'success' => 'success',
+				'error'   => 'danger',
+				'block'   => 'warning',
+				'info'    => 'info',
+				'primary' => 'primary',
+				'warning' => 'warning',
+				'danger'  => 'danger',
+			];
+	
+			if (is_array($payload['messages'])) {
+				foreach ($payload['messages'] as $msg) {
+					$msg = ['text'=>$msg['msg'],'stay'=>$msg['sticky'],'type'=>$types[$msg['type']]];
+					
+					if ($msg['sticky'] == true) {
+						$msg['staytime'] = ($payload['pause_for_each'] * ($payload['initial_pause']++));
+					}
+					
+					$msgs[] = $msg;
 				}
-				$msgs[] = $msg;
 			}
 		}
 
-		ci('page')->js_variable('messages',(array)$msgs);
+		ci('page')->js_variable('messages',$msgs);
 
 		return $msgs;
 	}
