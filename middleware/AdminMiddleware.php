268a9c46135083e6624f6d956320cc3e837330e8
Don Myers <donmyers@icloud.com>
2019-02-04 14:28:24 -0500

    update to add type hinting

--- middleware/AdminMiddleware.php
+++ middleware/AdminMiddleware.php
@@ -1,26 +1,34 @@
 <?php
 /**
- * AdminMiddleware
- * Insert description here
+ * Orange
+ *
+ * An open source extensions for CodeIgniter 3.x
+ *
+ * This content is released under the MIT License (MIT)
+ * Copyright (c) 2014 - 2019, Project Orange Box
+ */
+
+/**
+ * Admin Middleware 
+ *
+ * Handles permissions checking as well as loading additional classes as needed
  *
  * @package CodeIgniter / Orange
  * @author Don Myers
- * @copyright 2018
+ * @copyright 2019
  * @license http://opensource.org/licenses/MIT MIT License
  * @link https://github.com/ProjectOrangeBox
- * @version 2.0
+ * @version v2.0.0
  *
- * required
- * core:
- * libraries:
- * models:
- * helpers:
- * functions:
+ * @uses # load - CodeIgniter Loader 
  *
  */
 class AdminMiddleware extends Middleware_base {
 	public function request() : void
 	{
+		/**
+		 * Create the URL key and test if they have permissions
+		 */
 		$this->load->library('auth');
 
 		$key = 'url::/'.strtolower($this->router->fetch_directory().$this->router->fetch_class(true).'::'.$this->router->fetch_method(true).'~'.$this->router->fetch_request_method());
--- middleware/PublicMiddleware.php
+++ middleware/PublicMiddleware.php
@@ -21,5 +21,56 @@
 class PublicMiddleware extends Middleware_base {
 	public function request() : void
 	{
+		/**
+		 * Autoload:
+		 * Libraries
+		 * Models
+		 * Helpers
+		 * Model catalogs
+		 * Controller Default Model
+		 *
+		 * This really isn't needed anymore with the use of ci() which will autoload if it's not already loaded
+		 */
+		if ($this->libraries) {
+			$this->load->library((array) $this->libraries);
+		}
+
+		if ($this->models) {
+			$this->load->model((array) $this->models);
+		}
+
+		if ($this->helpers) {
+			$this->load->helpers((array) $this->helpers);
+		}
+
+		if ($this->catalogs) {
+			foreach ($this->catalogs as $variable_name=>$args) {
+				if (!is_array($args)) {
+					$model_name = $args;
+					$args = [];
+				} else {
+					$model_name = $args['model'];
+				}
+
+				$this->load->model($model_name);
+
+				$model_method = (isset($args['method'])) ? $args['method'] : 'catalog';
+
+				if (method_exists($this->$model_name,$model_method)) {
+					if ($model_method == 'catalog') {
+						$this->load->vars($variable_name, $this->$model_name->$model_method(@$args['array_key'],@$args['select'],@$args['where'],@$args['order_by'],@$args['cache'],@$args['with_deleted']));
+					} else {
+						$this->load->vars($variable_name, $this->$model_name->$model_method($args));
+					}
+				} else {
+					throw new Exception('Method "'.$model_method.'" doesn\'t exist on "'.$model_name.'"');
+				}
+			}
+		}
+
+		if ($this->controller_model) {
+			$this->load->model(strtolower($this->controller_model));
+		}
+
 	}
 }
