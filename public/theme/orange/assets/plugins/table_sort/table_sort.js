7c6528896ba94314e96da65951bbbaca818eab5f
Don Myers <donmyers@icloud.com>
2019-04-24 10:22:02 -0400

    updates to tablesort

--- public/theme/orange/assets/plugins/table_sort/table_sort.js
+++ public/theme/orange/assets/plugins/table_sort/table_sort.js
@@ -1,23 +1,32 @@
-/**
- *
- * Create the object to hold the properties and methods
- *
- */
-var table_sort = {};
+/* Create the object to hold the properties and methods */
+var tableSort = {};
 
-/**
- *
- * Default direction
- *
- */
-table_sort.dir = 'desc';
+/* Default direction */
+tableSort.dir = 'desc';
+tableSort.index = 0;
+tableSort.iconsAdded = false;
 
-/**
- *
- * Do the actual sort
- *
- */
-table_sort.sort = function(index,dir) {
+/* Do the actual sort */
+tableSort.sort = function(index,dir) {
+	if (index > 0) {
+		console.log('Sorting column:'+index+' direction:'+dir+' sorting:'+$('table.table-sort tbody tr').length);
+
+		tableSort.determineIcons(index,dir);
+
+		tinysort('table.table-sort tbody tr',{selector:'td:nth-child('+index+')',order:dir,data:'value'},{selector:'td:nth-child('+index+')',order:dir});
+	}
+
+	tableSort.save(index,dir);
+}
+
+tableSort.addSortIcons = function() {
+	if (!tableSort.iconsAdded) {
+		$('table.table-sort thead tr th:not(.nosort)').prepend('<i class="fa fa-sort"></i> ');
+		tableSort.iconsAdded = true;
+	}
+}
+
+tableSort.determineIcons = function(index,dir) {
 	/* remove all previous arrows and such */
 	$('table.table-sort thead tr th i').removeClass('fa-sort-asc').removeClass('fa-sort-desc').addClass('fa-sort');
 
@@ -28,57 +37,44 @@ table_sort.sort = function(index,dir) {
 	$('table.table-sort thead tr th:nth-child('+index+') i').addClass('fa-sort-'+dir).removeClass('fa-sort');
 
 	$('table.table-sort thead tr th:nth-child('+index+')').addClass('active');
-
-	/* do sort */
-	tinysort('table.table-sort tbody tr',{selector:'td:nth-child('+index+')',order:dir,data:'value'},{selector:'td:nth-child('+index+')',order:dir});
-
-	table_sort.save(index,dir);
 }
 
-/**
- *
- * Load the last sort if any
- *
- *
- */
- table_sort.load = function() {
+/* Load the last sort if any */
+ tableSort.load = function() {
 	var saved = $.jStorage.get(controller_path+'saved_sort_column','');
 
 	if (saved != '') {
 		parts = saved.split("\t");
 
-		table_sort.sort(parts[0],parts[1]);
+		tableSort.index = parts[0];
+		tableSort.dir = parts[1];
 	}
+
+	tableSort.sort(tableSort.index,tableSort.dir);
 }
 
-/**
- *
- * Save the Last Sort
- *
- *
- */
- table_sort.save = function(index,dir) {
+/* Save the Last Sort */
+ tableSort.save = function(index,dir) {
 	$.jStorage.set(controller_path+'saved_sort_column',index + "\t" + dir);
 }
 
-/**
- *
- * or sort if it's dynamically loaded
- *
- */
- $(document).on('orange_table_updated',{},function(tbody){
-	/**
-	 *
-	 * handle clicks
-	 *
-	 */
-	$('table.table-sort thead tr th:not(.nosort)').prepend('<i class="fa fa-sort"></i> ').on('click',function() {
-		/* which direction are we going now? */
-		table_sort.dir = (table_sort.dir == 'asc') ? 'desc' : 'asc';
+document.addEventListener("DOMContentLoaded",function(e){
+	tableSort.addSortIcons();
 
-		/* find out which column we clicked and send in the dir */
-		table_sort.sort($('table.table-sort thead tr th').index(this) + 1,table_sort.dir);
+	/* or sort if it's dynamically loaded */
+	$(document).on('orange_table_updated',{},function(tbody){
+		tableSort.load();
 	});
 
-	table_sort.load();
+	/* handle clicks */
+	$('table.table-sort thead tr th:not(.nosort)').on('click',function() {
+		/* which direction are we going now? */
+		tableSort.dir = (tableSort.dir == 'asc') ? 'desc' : 'asc';
+		tableSort.index = $('table.table-sort thead tr th').index(this) + 1;
+
+		/* find out which column we clicked and send in the dir */
+		tableSort.sort(tableSort.index,tableSort.dir);
+	});
+
+	tableSort.load();
 });
