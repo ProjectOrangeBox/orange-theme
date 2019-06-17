e138462fa9e3559df9d65e833b296f87e5cf4354
Don Myers <donmyers@icloud.com>
2019-06-17 12:13:02 -0400

    remove the cdn stuff

--- views/_templates/orange_admin.php
+++ views/_templates/orange_admin.php
@@ -3,16 +3,11 @@
 <?php pear::plugins('confirm_dialog,flash_msg,form_helpers,input_mask,keymaster,rest_form,select3,table_sticky_header,tab_save,table_sort,table_remember_position,notify,bootbox') ?>
 
 <?php pear::section('page_js') ?>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jStorage/0.4.12/jstorage.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.11/handlebars.min.js"></script>
 <!-- https://cdnjs.com/ -->
 <?php pear::parent() ?>
 <?php pear::end() ?>
 
 <?php pear::section('page_css') ?>
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 <link href="//fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 <?php pear::parent() ?>
--- views/_templates/orange_default.php
+++ views/_templates/orange_default.php
@@ -1,17 +1,13 @@
+<?php pear::asset_merge() ?>
+
 <?php pear::plugins('flash_msg,form_helpers,rest_form') ?>
 
 <?php pear::section('page_js') ?>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jStorage/0.4.12/jstorage.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.11/handlebars.min.js"></script>
 <!-- https://cdnjs.com/ -->
 <?php pear::parent() ?>
 <?php pear::end() ?>
 
 <?php pear::section('page_css') ?>
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 <link href="//fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 <?php pear::parent() ?>
 <?php pear::end() ?>
